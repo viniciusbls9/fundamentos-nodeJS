e52c37d68cff04c3152a49ddf634aeb58f6a9e15
Vinicius Benedito <beneditoloura@gmail.com>
2022-07-10 11:44:39 -0300

    ✨ feat: create verifyIfExistsAccountCpf middleware

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ app.use(express.json())
 
 const customers = []
 
+function verifyIfExistsAccountCpf(request, response, next) {
+  const { cpf } = request.headers
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if (!customer) {
+    return response.status(400).json({ error: 'Customer not found' })
+  }
+
+  request.customer = customer
+
+  return next()
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body
 
@@ -29,14 +43,8 @@ app.post("/account", (request, response) => {
 
 })
 
-app.get("/statement/:cpf", (request, response) => {
-  const { cpf } = request.params
-
-  const customer = customers.find(customer => customer.cpf === cpf)
-
-  if (!customer) {
-    return response.status(400).json({ error: 'Customer not found' })
-  }
+app.get("/statement", verifyIfExistsAccountCpf, (request, response) => {
+  const { customer } = request
 
   return response.json(customer.statement)
 })
