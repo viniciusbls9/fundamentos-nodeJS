6b6f232a0f320128caba16d023078d1c04e57f05
Vinicius Benedito <beneditoloura@gmail.com>
2022-07-10 11:59:16 -0300

    ✨ feat: create withdraw route

--- src/index.js
+++ src/index.js
@@ -21,6 +21,18 @@ function verifyIfExistsAccountCpf(request, response, next) {
   return next()
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === 'credit') {
+      return acc + operation.amount
+    } else {
+      return acc - operation.amount
+    }
+  }, 0)
+
+  return balance
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body
 
@@ -64,6 +76,28 @@ app.post("/deposit", verifyIfExistsAccountCpf, (request, response) => {
   customer.statement.push(statementOperation)
 
   return response.status(201).send()
+})
+
+app.post("/withdraw", verifyIfExistsAccountCpf, (request, response) => {
+  const { amount } = request.body
+  const { customer } = request
+
+  const balance = getBalance(customer.statement)
+
+  if (balance < amount) {
+    return response.status(400).json({ error: 'Insuficient funds' })
+  }
+
+  const statementOperation = {
+    amount,
+    create_at: new Date(),
+    type: 'debit'
+  }
+
+  customer.statement.push(statementOperation)
+
+  return response.status(201).send()
+
 })
 
 app.listen(3333)
