d2dfd1d1caceab5f4a00abbcf00ef4f16858cbf1
Vinicius Benedito <beneditoloura@gmail.com>
2022-07-10 11:45:51 -0300

    ✨ feat: create deposit route

--- src/index.js
+++ src/index.js
@@ -49,4 +49,21 @@ app.get("/statement", verifyIfExistsAccountCpf, (request, response) => {
   return response.json(customer.statement)
 })
 
+app.post("/deposit", verifyIfExistsAccountCpf, (request, response) => {
+  const { description, amount } = request.body
+
+  const { customer } = request
+
+  const statementOperation = {
+    description,
+    amount,
+    create_at: new Date(),
+    type: 'credit'
+  }
+
+  customer.statement.push(statementOperation)
+
+  return response.status(201).send()
+})
+
 app.listen(3333)
