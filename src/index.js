aa2c27ba00aba3fd73e1f8320d5ce4ea182c4e2a
Vinicius Benedito <beneditoloura@gmail.com>
2022-07-12 07:05:24 -0300

    ✨ feat: create update account route

--- src/index.js
+++ src/index.js
@@ -113,4 +113,13 @@ app.get("/statement/date", verifyIfExistsAccountCpf, (request, response) => {
   return response.json(statement)
 })
 
+app.put("/account", verifyIfExistsAccountCpf, (request, response) => {
+  const { name } = request.body
+  const { customer } = request
+
+  customer.name = name
+
+  return response.status(201).send()
+})
+
 app.listen(3333)
